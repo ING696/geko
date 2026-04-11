6592aa62c4998f6d091652ee867cfb1d6026ba6a
ING696 <inga.shahbazyan2004@gmail.com>
2026-04-11 21:23:46 +0400

    JS second class.

---
+++ classroom/javascript/script.js
@@ -0,0 +1,44 @@
+// JS 2 class
+// function sumOfNum(first_num, second_num) {
+//     console.log(`${first_num} + ${second_num} = ${first_num + second_num}`)
+// }
+
+// sumOfNum(10,20)
+
+// function numbersSum(...numbers) {
+//     let sum = 0
+//     for(let num in numbers) {
+//         sum += num
+//     }
+//     console.log(sum)
+// }
+
+// numbersSum(1,2,3,4,5,6,7,8,9,10)
+
+// const calcAge = function(year){
+//     return new Date().getFullYear() - year
+// }
+
+// console.log(calcAge(-782))
+
+// const calcAge = (year) => new Date().getFullYear() - year
+
+// console.log(calcAge(-782))
+
+// function factorial(num) {
+//     let p = 1;
+//     if(num < 2) {
+//         return 1
+//     }
+
+//         for(let i = num; i>0; i--) {
+//             p *= i
+//         }
+
+//     return `${num}! = ${p}`                                                                                                                                     
+// }
+
+// console.log(factorial(0))
+// console.log(factorial(4))
+
+
