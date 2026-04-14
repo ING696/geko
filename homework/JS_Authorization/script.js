2f05b64c3608fcdb9f4933e66ca790d20a43af30
ING696 <inga.shahbazyan2004@gmail.com>
2026-04-14 21:20:20 +0400

    Classroom 16.

---
+++ homework/JS_Authorization/script.js
@@ -0,0 +1,24 @@
+// const users =[
+//     {
+//         username: "admin",
+//         password: "1234",
+//         isBlocked: false
+//     },
+//     {
+//         username: "john",
+//         password: "querty",
+//         isBlocked: true
+//     },
+//     {
+//         username: "anna",
+//         password: "1234",
+//         isBlocked: false
+//     } 
+// ]
+
+// function login(users, username, password){}
+
+let arr = [1, 2, 3, 4, 5]
+let res = arr.sort()
+
+console.log(res)
