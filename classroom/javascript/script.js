cc5627f9dcdbc239e58e81742d935b18cc045319
ING696 <inga.shahbazyan2004@gmail.com>
2026-04-25 21:17:00 +0400

    Circle following mouse.

--- classroom/javascript/script.js
+++ classroom/javascript/script.js
@@ -1,4 +1,10 @@
-document.getElementById("btn").onclick = () => {
-  document.body.style.backgroundColor =
-    `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
-};
+// document.getElementById("btn").onclick = () => {
+//   document.body.style.backgroundColor =
+//     `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
+// };
+  const cursor = document.querySelector(".cursor");
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
